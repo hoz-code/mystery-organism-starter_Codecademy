233d6744f8400592bd261ef67a2f401eb60eff02
hoz-code <hozkar50@gmail.com>
2023-03-12 19:23:54 -0500

    Final Commit

--- mystery-organism-starter/main.js
+++ mystery-organism-starter/main.js
@@ -1,4 +1,4 @@
-let numRegister = [123456, 123457];
+let numRegister = [];
 // Returns a random DNA base
 const returnRandBase = () => {
   const dnaBases = ['A', 'T', 'C', 'G'];
@@ -13,17 +13,6 @@ const mockUpStrand = () => {
   }
   return newStrand;
 };
-
-
-
-pAequorFactory = () => {
-  return {
-    specimenNum: getNumId(),
-    dna: mockUpStrand()
-  }
-};
-
-
 
 const getNumId = () => {
   const randomId = () => {
@@ -50,6 +39,61 @@ const getNumId = () => {
 };
 
 
+
+pAequorFactory = () => {
+  return {
+    specimenNum: getNumId(),
+    dna: mockUpStrand(),
+    mutate() {
+      let numBases = (this.dna.length) - 1;
+      let basetoMutate = Math.floor(Math.random() * (numBases - 0 + 1) + 0);
+      const dnaBases = ['A', 'T', 'C', 'G'];
+      let indextoquit = dnaBases.findIndex((basetoFind) => basetoFind === this.dna[basetoMutate]);
+      dnaBases.splice(indextoquit, 1);
+      basetoChange = Math.floor(Math.random() * (((dnaBases.length) - 1) - 0 + 1) + 0);
+      this.dna.splice(basetoMutate, 1, dnaBases[basetoChange]);
+      return this.dna;
+    },
+    compareDNA(pAequor) {
+      let basesMatches = 0;
+      for (let i = 0; i <= this.dna.length; i++) {
+        if (this.dna.length[i] === pAequor.dna[i]) {
+          basesMatches++;
+        }
+      }
+      console.log(`specimen ${this.specimenNum} and specimen ${pAequor.specimenNum} have ${Math.round(basesMatches / 15 * 100)}% DNA in common`);
+    },
+    willLikelySurvive() {
+      let amountCorG = 0;
+      this.dna.forEach((base) => {
+        if (base === 'C' || base === 'G') {
+          amountCorG++;
+        }
+      });
+      if (amountCorG / this.dna.length >= 0.6) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  };
+};
+
+
+
+const pAequorGenerator = (numReplication) => {
+  let pAequorForStudy = [];
+  for (let i = 1; i <= numReplication; i++) {
+    pAequorForStudy.push(pAequorFactory());
+  }
+  return pAequorForStudy;
+};
+
+console.log(
+
+  pAequorGenerator(3)
+
+);
 
 
 
